f13ba46d156eaccb8a50c802c79cdece62550c34
YoungPad <martinmaeland@outlook.com>
2020-10-20 09:28:57 +0200

    Created mas221 repo

---
+++ robot_sim/robot/robot.py
@@ -0,0 +1,23 @@
+
+
+class Robot:
+
+    def __init__(self, name):
+        self.name = name
+        self.T_components = []
+
+    def convertDHtoT(dh): # converts dh table to t matrix
+        return T
+
+    def addTMatrix(T): # append t matrix to self
+        T_components.append(t)
+
+    def createTMatrix():
+        for t in self.T_components:
+
+
+    def getTMatrix(): # get t matrix which is product of all t matrices
+        pass
+
+    def plotRobot():
+        pass
---
+++ robot_sim/robot_sim.py
@@ -0,0 +1,5 @@
+from robot.robot import Robot
+
+robot1 = Robot("Martin")
+
+print(robot1.name)
