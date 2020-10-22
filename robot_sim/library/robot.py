0f0935f2c338766a4b8be4edf6eeabf1beb3926b
YoungPad <martinmaeland@outlook.com>
2020-10-23 00:01:36 +0200

    tested with second robot, works fine. next step will be to implement inverse-kinematics and animation of given set of points

--- robot_sim/library/robot.py
+++ robot_sim/library/robot.py
@@ -8,10 +8,16 @@ import math
 class Robot:
 
     def __init__(self):
-        self.dh_table = [] # this contains the dh-table
-        self.t_n = [] # this will contain all t_n matrices
-        self.t_matrix = [] # this will be the t-matrix
+        self.variables = [] # this is all the variables
+        self.variable_values = [] #this is current variable values
+        self.dh_table = [] # this represents dh-table
+        self.t_n = [] # this contains all t_n matrices
+        self.t_matrix = [] # this is the t-matrix
 
+    def initiate_variables(self, vars):
+        for var in vars:
+            var = sym.symbols("{}".format(var))
+            self.variables.append(var)
 
     def generate_dh_table(self, dh):
         for row in dh:
@@ -28,36 +34,31 @@ class Robot:
 
         self.t_matrix = t_matrix
 
-
-    def plot(self):
-
-        q1, q2 = sym.symbols("q1 q2")
+    def plot(self, var_values):
 
         # Initiate figure
         fig = plt.figure()
         ax = fig.gca(projection='3d')
 
         # Initial values
-        #last_point = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
         last_point = self.t_n[0]
         current_point = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
         last_xyz = [0,0,0]
 
         # Plotting data
-        #for i in (range(len(self.t_n))):
         for i in (range(len(self.t_n))):
 
-            #current_point = mm(self.t_n[-(i+1)], last_point)
             current_point = mm(last_point, current_point)
 
-            f = sym.lambdify([q1, q2], current_point[0][3], "numpy")
-            x = f(0, 0)
+            f = sym.lambdify(self.variables, current_point[0][3], "numpy")
+            #x = f(0, 0)
+            x = f(*var_values)
             print("x_{}: {}".format(i,x))
-            g = sym.lambdify([q1, q2], current_point[1][3], "numpy")
-            y = g(0, 0)
+            g = sym.lambdify(self.variables, current_point[1][3], "numpy")
+            y = g(*var_values)
             print("y_{}: {}".format(i,y))
-            h = sym.lambdify([q1, q2], current_point[2][3], "numpy")
-            z = h(0, 0)
+            h = sym.lambdify(self.variables, current_point[2][3], "numpy")
+            z = h(*var_values)
             print("z_{}: {}".format(i,z))
 
             ax.plot([last_xyz[0], x], [last_xyz[1], y], [last_xyz[2], z], color="black", zorder=1)
@@ -73,7 +74,7 @@ class Robot:
         ax.set_ylabel('y-axis')
         ax.set_zlabel('z-axis')
 
-        axis_limts = 2
+        axis_limts = 4
 
         ax.set_xlim(-axis_limts,axis_limts)
         ax.set_ylim(-axis_limts,axis_limts)
--- robot_sim/robot_sim.py
+++ robot_sim/robot_sim.py
@@ -1,11 +1,13 @@
 from library.robot import Robot
-from library.tools import *
+#from library.tools import *
 from sympy import symbols
-import sympy as sym
-import math
+#import sympy as sym
+#import math
 
+#----ROBOT 1----
 # Define symbolic variables
 q1, q2 = symbols("q1 q2")
+variables = [q1, q2]
 
 # Define link lengths
 l1 = 1
@@ -14,20 +16,36 @@ l2 = 1
 # Define DH-table
 dh_1 = [q1 + 0.7853981634, 0, l1, 0]
 dh_2 = [q2 - 0.7853981634, 0, l2, 0]
-
 dh = [dh_1, dh_2]
+
+# Give variables some values
+values = [0, 0]
 
 # Define robot
 robot = Robot()
+robot.initiate_variables(variables)
 robot.generate_dh_table(dh)
 robot.generate_t_matrix()
-robot.plot()
+robot.plot(values)
 
-# TESTING
 
-t1 = mm(rotZ(0.7853981634), transX(l1))
-t2 = transX(l2)
-T = mm(t1,t2)
+#----ROBOT2----
+# Define symbolic variables
+q1, d1 = symbols("q1 d1")
+variables_2 = [q1, d1]
 
-print(robot.t_n[0])
-print(t1)
+# Define DH-table
+dh2_1 = [q1, 0, l1, 0]
+dh2_2 = [-0.785398163, 0, 0, -1.57079633]
+dh2_3 = [0, d1, 0, 0]
+dh2 = [dh2_1, dh2_2, dh2_3]
+
+# Give variables some values
+values_2 = [0.1, 2]
+
+# Define test robot
+robot2 = Robot()
+robot2.initiate_variables(variables_2)
+robot2.generate_dh_table(dh2)
+robot2.generate_t_matrix()
+robot2.plot(values_2)
