fdbd18c799f11009ddabb6c1daecbd30af5cb63d
Martin <martinmaeland@outlookl.com>
2020-10-24 15:24:06 +0200

    minor changes

--- robot_sim/robot_sim.py
+++ robot_sim/robot_sim.py
@@ -11,7 +11,7 @@ variables = [q1, q2]
 
 # Define link lengths
 l1 = 1
-l2 = 1
+l2 = 2
 
 # Define DH-table
 dh_1 = [q1 + 0.7853981634, 0, l1, 0]
@@ -27,25 +27,3 @@ robot.initiate_variables(variables)
 robot.generate_dh_table(dh)
 robot.generate_t_matrix()
 robot.plot(values)
-
-
-#----ROBOT2----
-# Define symbolic variables
-q1, d1 = symbols("q1 d1")
-variables_2 = [q1, d1]
-
-# Define DH-table
-dh2_1 = [q1, 0, l1, 0]
-dh2_2 = [-0.785398163, 0, 0, -1.57079633]
-dh2_3 = [0, d1, 0, 0]
-dh2 = [dh2_1, dh2_2, dh2_3]
-
-# Give variables some values
-values_2 = [0.1, 2]
-
-# Define test robot
-robot2 = Robot()
-robot2.initiate_variables(variables_2)
-robot2.generate_dh_table(dh2)
-robot2.generate_t_matrix()
-robot2.plot(values_2)
